ab5d833e6a2aeb3884865d4c41566e777a51124c
jeerawut <jeerawut.te@gmail.com>
2023-12-28 07:38:21 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,13 @@ import "../../public/scss/styles.global.scss"
 import Header from './component/header'
 import Footer from './component/footer'
 import Script from 'next/script'
+export const metadata = {
+  title: 'EZorder | ระบบสั่งอาหารและจัดการเมนู',
+  description: 'EZorder คือ ลูกค้าสั่งอาหารได้ผ่านการสแกน QR code บนมือถือ ซึ่งจะช่วยลดเวลาในการรอคิวและช่วยให้ลูกค้าสั่งอาหารได้อย่างรวดเร็วในราคาคุ้มค่า ประหยัดต้นทุนในการทำงาน บริหารร้านได้สะดวกสบายและง่ายยิ่งขึ้น',
+}
 
 export default function RootLayout({ children }) {
+  
   return (
     <html lang="en">
 
