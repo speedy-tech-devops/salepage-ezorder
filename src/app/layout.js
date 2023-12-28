a6082aeb24fa28d51ce186d02db9b14291a8c8ee
jeerawut <jeerawut.te@gmail.com>
2023-12-28 07:33:05 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,23 @@ import "../../public/scss/styles.global.scss"
 
 import Header from './component/header'
 import Footer from './component/footer'
+import Script from 'next/script'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
       <body>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-7X2YPE0DKN" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-7X2YPE0DKN');
+        `}
+      </Script>
         <Header />
         {children}
         <Footer />
